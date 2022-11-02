2e42f2df118c59a36a688975c7deab90ce85f164
Sam Mousa <sam@mousa.nl>
2022-11-02 17:09:53 +0100

    chore: fix ecs config

--- ecs.php
+++ ecs.php
@@ -5,29 +5,16 @@ declare(strict_types=1);
 // ecs.php
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-    // Parallel
-    $parameters->set(Option::PARALLEL, true);
-
-    // Paths
-    $parameters->set(Option::PATHS, [
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->parallel();
+    $ecsConfig->paths([
         __DIR__ . '/src', __DIR__ . '/tests', __DIR__ . '/ecs.php'
-
-
-
     ]);
-    // A. full sets
-    $containerConfigurator->import(SetList::PSR_12);
+    $ecsConfig->sets([SetList::PSR_12]);
 
-
-    // B. standalone rule
-    $services = $containerConfigurator->services();
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]]);
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, ['syntax' => 'short']);
 };
